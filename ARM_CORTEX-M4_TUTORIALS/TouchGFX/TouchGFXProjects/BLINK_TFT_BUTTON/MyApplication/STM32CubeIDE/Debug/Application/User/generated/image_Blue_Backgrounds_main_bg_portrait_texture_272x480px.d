fc2e574d1930bd9ef09185e29a207a02a712730d
Ajay Mathews <37361585+ajaymathews@users.noreply.github.com>
2023-03-25 16:43:37 +0100

    Add files via upload

---
+++ ARM_CORTEX-M4_TUTORIALS/TouchGFX/TouchGFXProjects/BLINK_TFT_BUTTON/MyApplication/STM32CubeIDE/Debug/Application/User/generated/image_Blue_Backgrounds_main_bg_portrait_texture_272x480px.d
@@ -0,0 +1,5 @@
+Application/User/generated/image_Blue_Backgrounds_main_bg_portrait_texture_272x480px.o: \
+ C:/TouchGFXProjects/MyApplication/TouchGFX/generated/images/src/__designer/image_Blue_Backgrounds_main_bg_portrait_texture_272x480px.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
---
+++ ARM_CORTEX-M4_TUTORIALS/TouchGFX/TouchGFXProjects/BLINK_TFT_BUTTON/MyApplication/STM32CubeIDE/Debug/Application/User/generated/image_Dark_Buttons_Round_small_pressed.d
@@ -0,0 +1,5 @@
+Application/User/generated/image_Dark_Buttons_Round_small_pressed.o: \
+ C:/TouchGFXProjects/MyApplication/TouchGFX/generated/images/src/__designer/image_Dark_Buttons_Round_small_pressed.cpp \
+ ../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+../../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
