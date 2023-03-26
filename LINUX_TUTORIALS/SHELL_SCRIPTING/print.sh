f2f324e36a3dadfa1774c74d7a04407758e15f01
Ajay Mathews <37361585+ajaymathews@users.noreply.github.com>
2023-03-26 03:18:25 +0200

    Delete LINUX_TUTORIALS directory

--- LINUX_TUTORIALS/SHELL_SCRIPTING/print.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Running the print pgm"
-printf "th current location is $pwd "
-echo "the current folder elmnts are $ls"
--- LINUX_TUTORIALS/SHELL_SCRIPTING/variable_count.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-count=0
-while :
-do
-	count=$((count+1))
-	echo $count
-	sleep 1
-done
